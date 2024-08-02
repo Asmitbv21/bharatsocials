b97fcf34999ddf414e9dede3f26132e798dcc1f0
Viraj-2005 <141951331+Viraj-2005@users.noreply.github.com>
2024-08-02 14:00:59 +0530

    Made NGO lists

--- lib/domains/envSus.dart
+++ lib/domains/envSus.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
 
 class EnvSus extends StatelessWidget {
+  final List<Map<String, String>> ngos = [
+    {'name': 'Vidya NGO', 'location': 'Mumbai', 'color': '0xFFCCDBCC'},
+    {'name': 'Asha NGO', 'location': 'Pune', 'color': '0xFFF4CCCC'},
+    {'name': 'Disha Foundation', 'location': 'Satara', 'color': '0xFFD9D2E9'},
+    {'name': 'Snehalaya', 'location': 'Pune', 'color': '0xFFE6E6E6'},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Environment and Sustainability'),
+        title: Text('Environment and Sustainabilty'),
       ),
-      body: Center(
-        child: Text('This is the Environment and Sustainability page'),
+      body: ListView.builder(
+        itemCount: ngos.length,
+        itemBuilder: (context, index) {
+          return Container(
+            decoration: BoxDecoration(
+              color: Color(int.parse(ngos[index]['color']!)),
+              borderRadius: BorderRadius.circular(10), // Rounded corners
+            ),
+            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+            child: ListTile(
+              title: Text(
+                ngos[index]['name']!,
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              subtitle: Text('Location: ${ngos[index]['location']}'),
+              trailing: Icon(Icons.arrow_forward),
+              onTap: () {
+                // Navigate to profile screen
+              },
+            ),
+          );
+        },
       ),
     );
   }
